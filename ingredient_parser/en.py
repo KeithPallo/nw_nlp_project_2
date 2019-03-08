8d7a8d5127ac72dff199d56eccdd363ee2b5da91
Ikhlas <ikhlas.attarwala@gmail.com>
2019-03-08 00:54:21 -0600

    not finished

--- ingredient_parser/en.py
+++ ingredient_parser/en.py
@@ -1,62 +1,109 @@
-
 import re
-from itertools import chain
-
-from .utils import normalize, escape_re_string
 
 UNITS = {"cup": ["cups", "cup", "c.", "c"],
-         "fluid_ounce": ["fl. oz.", "fl oz", "fluid ounce", "fluid ounces"],
-         "gallon": ["gal", "gal.", "gallon", "gallons"],
-         "ounce": ["oz", "oz.", "ounce", "ounces"],
-         "pint": ["pt", "pt.", "pint", "pints"],
-         "pound": ["lb", "lb.", "pound", "pounds"],
-         "quart": ["qt", "qt.", "qts", "qts.", "quart", "quarts"],
-         "tablespoon": ["tbsp.", "tbsp", "T", "T.", "tablespoon", "tablespoons", "tbs.", "tbs"],
-         "teaspoon": ["tsp.", "tsp", "t", "t.", "teaspoon", "teaspoons"],
-         "gram": ["g", "g.", "gr", "gr.", "gram", "grams"],
-         "kilogram": ["kg", "kg.", "kilogram", "kilograms"],
-         "liter": ["l", "l.", "liter", "liters"],
-         "milligram": ["mg", "mg.", "milligram", "milligrams"],
-         "milliliter": ["ml", "ml.", "milliliter", "milliliters"],
-         "pinch": ["pinch", "pinches"],
-         "dash": ["dash", "dashes"],
-         "touch": ["touch", "touches"],
-         "handful": ["handful", "handfuls"],
-         "stick": ["stick", "sticks"],
-         "clove": ["cloves", "clove"],
-         "can": ["cans", "can"], "large": ["large"],
-         "small": ["small"],
-         "scoop": ["scoop", "scoops"],
-         "filets": ["filet", "filets"],
-         "sprig": ["sprigs", "sprig"]}
+		 "gill": ["gill", "gills"],
+		 "jack": ["jack", "jacks"],
+		 "bunch": ["bunch", "bunches"],
+		 "fluid ounce": ["fl. oz.", "fl oz", "fl oz.", "fl. oz", "fluid ounce", "fluid ounces"],
+		 "gallon": ["gal", "gal.", "gallon", "gallons"],
+		 "ounce": ["oz", "oz.", "ounce", "ounces"],
+		 "pint": ["pt", "pt.", "pint", "pints"],
+		 "fluid pint": ["fl pt", "fl. pt.", "fl. pt", "fl pt.", "fluid pint", "fluid pints"],
+		 "pound": ["lb", "lb.", "lbs", "lbs.", "pound", "pounds", "#"],
+		 "quart": ["qt", "qt.", "qts", "qts.", "quart", "quarts"],
+		 # NOTE: tablespoon can be T as teaspoon can be t
+		 "tablespoon": ["tbsp.", "tbsp", "T", "T.", "tablespoon", "tablespoons", "tbs.", "tbs"],
+		 # NOTE: teaspoon can be t as tablespoon can be T
+		 "teaspoon": ["tsp.", "tsp", "t", "t.", "teaspoon", "teaspoons"],
+		 "gram": ["g", "g.", "gr", "gr.", "gram", "grams", "gramme", "grammes"],
+		 "kilogram": ["kg", "kg.", "kilogram", "kilograms", "kilogramme", "kilogrammes"],
+		 "liter": ["l", "l.", "L", "L.", "liter", "liters", "litre", "litres"],
+		 "deciliter": ["dl", "dl.", "dL", "dL.", "deciliter", "deciliters", "decilitre", "decilitres"],
+		 "milligram": ["mg", "mg.", "milligram", "milligrams", "milligramme", "milligrammes"],
+		 "milliliter": ["ml", "ml.", "milliliter", "milliliters", "millilitre", "millilitres", "cc", "cc.", "CC", "CC.",
+						"Cc", "Cc.", "cC", "cC.", "mL", "mL."],
+		 "millimeter": ["mm", "mm.", "millimeter", "millimeters", "millimetre", "millimetres"],
+		 "centimeter": ["cm", "cm.", "centimeter", "centimeters", "centimetre", "centimetres"],
+		 "meter": ["m", "m.", "M", "M.", "meter", "meters", "metre", "metres"],
+		 "inch": ["inch", "inches"],
+		 "pinch": ["pinch", "pinches"],
+		 "dash": ["dash", "dashes"],
+		 "touch": ["touch", "touches"],
+		 "handful": ["handful", "handfuls"],
+		 "stick": ["stick", "sticks"],
+		 "clove": ["cloves", "clove"],
+		 "can": ["cans", "can"],
+		 "large": ["lar", "lar.", "large"],
+		 "small": ["sm", "sm.", "small"],
+		 "scoop": ["sc", "sc.", "scoop", "scoops"],
+		 "fillets": ["filet", "filets", "fillet", "fillets"],
+		 "sprig": ["sprig", "sprigs"],
+		 "bit": ["bit", "bits"],
+		 "few": ["few"],
+		 "some": ["some"]
+		 }
+
+# tools kb could go here
 
 NUMBERS = ['seventeen', 'eighteen', 'thirteen', 'nineteen', 'fourteen', 'sixteen', 'fifteen', 'seventy', 'twelve',
-           'eleven', 'eighty', 'thirty', 'ninety', 'twenty', 'seven', 'fifty', 'sixty', 'forty', 'three', 'eight',
-           'four', 'zero', 'five', 'nine', 'ten', 'one', 'six', 'two', 'an', 'a']
+		   'eleven', 'eighty', 'thirty', 'ninety', 'twenty', 'seven', 'fifty', 'sixty', 'forty', 'three', 'eight',
+		   'four', 'zero', 'five', 'nine', 'ten', 'one', 'six', 'two', 'an', 'a']
 
-prepositions = ["of"]
+# possible misspelling
+PREPOSITIONS = ["of", "off"]
 
-a = list(chain.from_iterable(UNITS.values()))
-a.sort(key=lambda x: len(x), reverse=True)
-a = map(escape_re_string, a)
+
+# a = list(chain.from_iterable(UNITS.values()))
+def test(rec):
+	for each in rec:
+		for one in each:
+			yield one
+
+
+def test2(*rec):
+	for each in rec:
+		for one in each:
+			yield one
+
+
+a = (test2(UNITS.values()))
+b = list(test(a))
+
+# not necessary?
+# b.sort(key=lambda x: len(x), reverse=True)
+
+# a = map(escape_re_string, a)
+# not working?
+for each in b:
+	each.replace('.', '\.')
+	each.replace('(', '')
+	each.replace(')', '')
+	re.sub(r'\s+', ' ', each)
 
 PARSER_RE = re.compile(
-    r'(?P<quantity>(?:[\d\.,][\d\.,\s/]*)?\s*(?:(?:%s)\s*)*)?(\s*(?P<unit>%s)\s+)?(\s*(?:%s)\s+)?(\s*(?P<name>.+))?' % (
-        '|'.join(NUMBERS), '|'.join(a), '|'.join(prepositions)))
+	r'(?P<quantity>(?:[\d\.,][\d\.,\s/]*)?\s*(?:(?:%s)\s*)*)?(\s*(?P<unit>%s)\s+)?(\s*(?:%s)\s+)?(\s*(?P<name>.+))?' % (
+		'|'.join(NUMBERS), '|'.join(b), '|'.join(PREPOSITIONS)))
+
+
+# PARSER_RE = re.compile(
+# 	r'(?P<quantity>(?:[(0-9)\.,][\d\.,\s/]*)?[\r\n\t\f\v ]*(?:(?:%s)[\r\n\t\f\v ]*)*)?([\r\n\t\f\v ]*(?P<unit>%s)[\r\n\t\f\v ]+)?([\r\n\t\f\v ]*(?:%s)[\r\n\t\f\v ]+)?([\r\n\t\f\v ]*(?P<name>.+))?' % (
+# 	'|'.join(NUMBERS), '|'.join(b), '|'.join(PREPOSITIONS)))
 
 
 def parse(st):
-    """
+	"""
 
-    :param st:
-    :return:
-    """
-    st = normalize(st)
-    res = PARSER_RE.match(st)
+	:param st:
+	:return:
+	"""
+	# st = normalize(st)
+	st = re.sub(r'\s', ' ', re.compile(r'^([\d\s*[\d\.,/]*)\s*(.+)').sub('\g<1> \g<2>', st)).strip()
+	res = PARSER_RE.match(st)
 
-    return {
-        'quantity':(res.group('quantity') or '').strip(),
-        'unit':(res.group('unit') or '').strip(),
-        'measure': (res.group('quantity') or '').strip() + ' ' + (res.group('unit') or '').strip(),
-        'name': (res.group('name') or '').strip()
-    }
+	return {
+		'Name': (res.group('name') or '').strip().lower(),
+		'Quantity': (res.group('quantity') or '').strip().lower(),
+		# 'Measurement': (res.group('quantity') or '').strip() + ' ' + (res.group('unit') or '').strip(),
+		'Units': (res.group('unit') or '').strip(),
+		# 'Tools': (res.group('tools') or '').strip().lower(),
+	}
--- ingredient_parser/utils.py
+++ ingredient_parser/utils.py
@@ -1,16 +1,14 @@
 import re
 
 
-SEPARATOR_RE = re.compile(r'^([\d\s*[\d\.,/]*)\s*(.+)')
-
-
 def normalize(st):
     """
 
     :param st:
     :return:
     """
-    return re.sub(r'\s+', ' ', SEPARATOR_RE.sub('\g<1> \g<2>', st)).strip()
+    # SEPARATOR_RE = re.compile(r'^([\d\s*[\d\.,/]*)\s*(.+)')
+    return re.sub(r'\s+', ' ', re.compile(r'^([\d\s*[\d\.,/]*)\s*(.+)').sub('\g<1> \g<2>', st)).strip()
 
 
 def escape_re_string(text):
