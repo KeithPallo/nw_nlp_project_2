d84e3b77a965289766d2619ce275231e375353a8
Keith Pallo <keithpallo@dhcp-10-105-4-123.wireless.northwestern.private>
2019-03-08 00:04:02 -0600

    shell and prgress

--- main_run.py
+++ main_run.py
@@ -1,17 +1,15 @@
 # import functions from other python files
 
+import healthy
 
 
 # python3 -c 'import main; main.run_interface()'
 
-# Potential for importing Jupyter notebook
-
+# Potential for importing Jupyter notebook - have not tested this out
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
-
 from IPython.display import display, HTML
-
 formatter = HtmlFormatter()
 
 
@@ -19,34 +17,40 @@ def run_interface():
 
     # prompt user to input a URL
 
-    # give options
+    # parse url using parse_url.py
+
+    # make original
 
     # while user wants to continue
-        # select transform
 
-        # call transform
+        # display original recipe for user
 
-        # print transform out
+        # give options for transforms
+
+        # select transform based on input
+
+        # call transform from imported files
+
+        # print new recipe
+
+        # print differences using diff_for_display
+
+
+
         # write transform to txt file for later processing
 
 def test_internal():
 
     # read in list of URLS
 
-    # display current URL
-
-    # while we want to continie
-
-        # run transforms
-
-        # print to screen
+    # call run interface with optional parameter to input parameter
 
 
 def display_standard():
     # display ingredients for the user
 
 def diff_for_display(original_list,old_list):
-    # after printing the new version
+    # after printing the new version - use this to display the
 
 
 def parse_ingredients():
