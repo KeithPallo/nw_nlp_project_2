7d24732d8495d7e5f630ff13915115b8e9ec38c3
Ikhlas <ikhlas.attarwala@gmail.com>
2019-03-10 16:28:36 -0500

    123 all done

--- ing_parser.py
+++ ing_parser.py
@@ -1,9 +1,8 @@
 import re
 import json
-from pprint import pprint
 
-with open('ingredients_kb.json') as f:
-	ingredients = json.load(f)
+# with open('ingredients_kb.json') as f:
+# 	ingredients = json.load(f)
 
 with open('quantity_kb.json') as f:
 	quantity = json.load(f)
@@ -11,11 +10,9 @@ with open('quantity_kb.json') as f:
 with open('measurements_kb.json') as f:
 	measurements = json.load(f)
 
-# tools kb could go here
-
-ing_list = []
-for each in ingredients:
-	ing_list.extend(ingredients[each])
+# ingredients_list = []
+# for each in ingredients:
+# 	ingredients_list.extend(ingredients[each])
 
 qty_list = []
 for each in quantity:
@@ -25,13 +22,11 @@ measurement_list = []
 for each in measurements:
 	measurement_list.extend(measurements[each])
 
-# print(measurement_list[1])
-
 ingredient_string = re.compile(
-	# Groups:
+	# Groups may have changed, but originally were:
 	# 1 = All #s quantity that's not in parentheses
 	# 2 = Whole # of Group 1
-	# 3/4 = Second whole # or denominator if only fraction
+	# 3/4 = Second whole # or full fraction
 	# 5/6 = Measurements
 	# 7/8 = n/a of Measurements
 	# 9/10 = Name
@@ -46,40 +41,56 @@ ingredient_string = re.compile(
 	r'(\s?(%s)\s+)?'
 	# 9/10
 	r'(\s?(.+))?'
-	% ('|'.join(qty_list), '|'.join(measurement_list), '|'.join(["of", "off", "to"])))
+	% ('|'.join(qty_list),
+	   '|'.join(measurement_list),
+	   '|'.join(["of", "off"])))
 
-
-# ingredient_string = re.compile(
-# 	r'(?P<quantity>([\d\.,][\d\.,\s/]*)?[\r\n\t\f\v ]*((%s)[\r\n\t\f\v ]*)*)?([\r\n\t\f\v ]*(?P<unit>%s)[\r\n\t\f\v ]+)?([\r\n\t\f\v ]*(%s)[\r\n\t\f\v ]+)?([\r\n\t\f\v ]*(?P<name>.+))?' % (
-# 	'|'.join(NUMBERS), '|'.join(b), '|'.join(PREPOSITIONS)))
-
-# descriptro = if not in ingredients kb, adjective?
-# preparation = verb phrase
 
 def parse(string):
-	"""
-
-	:param st:
-	:return:
-	"""
-	# st = normalize(st)
-	string = re.sub(r'\s', ' ', re.compile(r'^([\d\s*[\d\.,/]*)\s*(.+)').sub('\g<1>\g<2>', string)).strip()
 	reg = ingredient_string.match(string)
 
-	# in name: has (??) at front & measurement == '', measurement = (??)
-	# in name: if phrase in ing_list, return it, else return the whole name
-	# in name: parse descriptors as ??
-	# in name: parse preparation as verb phrase??
 	n = {'name': (reg.group(9) or '').strip().lower()}
-	# in quantity: check w/ miriam & jason
-	# if second # in quantity is an integer (whole #), keep that in name
 	q = {'quantity': (reg.group(1) or '').strip().lower()}
-	# in measurement: if name has (something) at front & measurement == '', measurement = (something) without paren
-	m = {'measurement': (reg.group(5) or '').strip()}
+	m = {'measure': (reg.group(6) or '').strip()}
+
+	# yet to do:
+	# if in name: if phrase in ingredients_list, return it, else return the whole name
+	# if in name: parse preparation as verb phrase
+	# if in name: parse descriptors as all else
+
+	# looks for parentheses in name
+	if '(' in reg.group(9) and ')' in reg.group(9):
+		mesPar = re.findall(r'(\(.*?\))', reg.group(9))
+		mesWO = mesPar[0][1:-1]
+		split2 = mesWO.split()
+		for each in split2:
+			if each in measurement_list:
+				remove_mes_from_name = reg.group(9).replace(mesPar[0], '')
+				n = {'name': (remove_mes_from_name or '').strip().lower()}
+				m = {'measure': (split2[0] + ' ' + split2[-1] or '').strip().lower()}
+
+	# if second # in quantity is a whole #, add it to measurement
+	if reg.group(1) != '':
+		splitQ = reg.group(1).split()
+		if splitQ[-1] == '':
+			splitQ = splitQ[0:-1]
+		if splitQ[0] == splitQ[-1]:
+			q = {'quantity': (reg.group(1) or '').strip().lower()}
+		else:
+			if '/' in str(splitQ[-1]):
+				q = {'quantity': (reg.group(1) or '').strip().lower()}
+			else:
+				q = {'quantity': (splitQ[0] or '').strip().lower()}
+				m = {'measure': (splitQ[-1] + ' ' + str(reg.group(6)) or '').strip()}
+	if m == {'measure': ''} or 'None' in m['measure'] and r'\d' not in m['measure']:
+		splitN = reg.group(9).split()
+		for each in splitN:
+			if each in measurement_list:
+				m = {'measure': str(each)}
+	if 'cups' in m.values():
+		print(True)
+	if q == {'quantity': ''}:
+		q = {'quantity': 'n/a'}
 	parsed = {**q, **n, **m}
 
 	return parsed
-
-# return {**n}
-# return {**q}
-# return {**m}
--- test.py
+++ test.py
@@ -1,50 +1,29 @@
 import re
 
-class Ingredients(??????????):
-	# ...
-	Name = ""
-	Qty = 0
-	Measure = 0
-	Details = ""
-	PrepStyle = ""
-	Methods = []
-	Toolset = []
-	PrefComb = []
+# string = '2 9/2 '
+# x = string.split(' ')
+# print(x[0])
+# print(x[-2])
+# print(x[-1])
+# print(x[0])
+# x = x[0:-1]
+# print(x)
+# if '/' in x[-1]:
+# 	print(True)
+# else:
+# 	print(False)
 
-	def __init__(self,
-				 Name="",
-				 Qty=0,
-				 Measure=0,
-				 Details="",
-				 PrepStyle="",
-				 Methods=[],
-				 Toolset=[],
-				 PrefComb=[],
-				 ):
-		self.Name = Name
-		self.Qty = Qty,
-		self.Measure = Measure
-		self.Details = Details
-		self.PrepStyle = PrepStyle
-		self.Methods = Methods
-		self.Toolset = Toolset
-		self.PrefComb = PrefComb
+st = '(9 inch) pizzas'
+x = ['inch', 'foot']
 
-	def printIngredient(self):
-		# print()
-		print("Name: ", self.Name)
-		if self.Qty:
-			print("Name: ", self.Qty)
-
-# meat_options
-meat_options = []
-
-PARSER_RE = re.compile(
-	r'(?P<quantity>(?:[\d\.,][\d\.,\s/]*)?\s*(?:(?:%s)\s*)*)'
-	r'?'
-	r'(\s*(?P<unit>%s)\s+)'
-	r'?'
-	r'(\s*(?:%s)\s+)'
-	r'?'
-	r'(\s*(?P<name>.+))?' % (
-	'|'.join(quantity), '|'.join(b), '|'.join(PREPOSITIONS))
+if '(' in st:
+	mes1 = re.findall(r'(\(.*?\))', st)
+	mes2 = mes1[0][1:-1]
+	print(mes1[0])
+	print(mes2)
+	split2 = mes2.split()
+	print(split2)
+	for each in split2:
+		if each in x:
+			print(split2[0])
+			print(split2[-1])
